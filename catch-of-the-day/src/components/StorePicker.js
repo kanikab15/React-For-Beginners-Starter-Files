04ceafd1991915083ef86861612b5f099c5cb0e1
Kanika B <kanika.b@gmail.com>
2020-08-09 01:50:33 +0530

    upto lesson 11 events and this binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+  }
+
   render(){
     return (
       <Fragment>
         { /* this is a comment */}
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store </h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input type="text" required
+            ref={this.myInput} placeholder="Store Name" defaultValue = {getFunName()}/>
           <button type="submit">Visit Store</button>
         </form>
       </Fragment>
